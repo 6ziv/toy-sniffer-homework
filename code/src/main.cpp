a1f71d18fa3218dad1768c8f293aeef3c275c253
6ziv <34367212+6ziv@users.noreply.github.com>
2022-04-07 16:56:39 +0800

    1. Add cmake for report generation 2. use npcap dlls first 3. github actions auto build

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -4,9 +4,39 @@
 #include <QTranslator>
 #include <aixlog.hpp>
 #include <memory>
+#include <Windows.h>
+#include <sysinfoapi.h>
+#include <pcap.h>
+void update_dir(){
+  UINT len_system = 0;
+  wchar_t *ptr = reinterpret_cast<wchar_t*>(malloc(sizeof(wchar_t)));
+  if(!ptr)return;
+  len_system = GetSystemDirectoryW(ptr,0);
+  free(ptr);
+  if(len_system==0)return;
+  
+  ptr = reinterpret_cast<wchar_t*>(malloc((len_system + 16)*sizeof(wchar_t)));
+  if(!ptr)return;
+  len_system = GetSystemDirectoryW(ptr,len_system);
+  if(len_system==0){
+	free(ptr);
+	return;
+  }
+  ptr[len_system] = L'\0';
+  wcscat(ptr,L"\\Npcap");
+  SetDllDirectoryW(ptr);
+  const char *pcap_version = pcap_lib_version();
+  //LOG(INFO)<<"PCAP Version="<<pcap_version;
+  
+}
 int main(int argc, char *argv[]) {
-  AixLog::Log::init({std::make_shared<AixLog::SinkFile>(AixLog::Severity::error,
-                                                        "error.log")});
+  
+  AixLog::Log::init({
+	  std::make_shared<AixLog::SinkFile>(AixLog::Severity::error,"error.log")
+	  ,std::make_shared<AixLog::SinkFile>(AixLog::Severity::warning,"warning.log")
+//	  ,std::make_shared<AixLog::SinkFile>(AixLog::Severity::info,"info.log")
+	  });
+  update_dir();
   QApplication a(argc, argv);
 
   QTranslator translator;
